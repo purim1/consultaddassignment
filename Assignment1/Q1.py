dafa13cd59f0e14bd957a4b3d438dbd051cc860c
macmini <purim1@montclair.edu>
2019-05-11 15:40:27 -0400

    change 1

--- Assignment1/Q1.py
+++ Assignment1/Q1.py
@@ -6,6 +6,6 @@ inp3 = float(raw_input())
 
 print (inp, inp2, inp3)
 
-print(type(inp)) # <type 'str'>
+print(type(inp)) # <type 'str'> dlfksdf
 print(type(inp2)) #<type 'str'>
 print(type(inp3)) #<type 'float'>
