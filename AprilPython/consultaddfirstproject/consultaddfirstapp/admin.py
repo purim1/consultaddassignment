0a71b0714521cdc8c340c3c271b9439ce6abb5c1
purim1 <50432464+purim1@users.noreply.github.com>
2019-05-16 10:22:16 -0400

    adding tables to admin

--- AprilPython/consultaddfirstproject/consultaddfirstapp/admin.py
+++ AprilPython/consultaddfirstproject/consultaddfirstapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from consultaddfirstapp.models import Candidate ,CandidateInfo
+from consultaddfirstapp.models import Candidate ,CandidateInfo, CandidateAddress, CandidateSalary
 # Register your models here.
 
 admin.site.register(Candidate)
 admin.site.register(CandidateInfo)
+admin.site.register(CandidateAddress)
+admin.site.register(CandidateSalary)
 
