fc1ac436be643c825ca454638514cd551e408e62
purim1 <50432464+purim1@users.noreply.github.com>
2019-05-16 10:22:38 -0400

    adding addr, salary to models

--- AprilPython/consultaddfirstproject/consultaddfirstapp/models.py
+++ AprilPython/consultaddfirstproject/consultaddfirstapp/models.py
@@ -5,10 +5,20 @@ from django.db import models
 class Candidate(models.Model):
     Name_of_Can=models.CharField(max_length=100,unique=True)
 
+    def __str__(self):
+        return self.Name_of_Can
+
 
 class CandidateInfo(models.Model):
     CanID=models.ForeignKey(Candidate,on_delete=models.PROTECT)
+    CandDept = models.CharField(max_length=150,unique=True)
 
 
 
+class CandidateAddress(models.Model):
+    CanAddr = models.CharField(max_length=150,unique=True)
+
+
+class CandidateSalary(models.Model):
+    CanSalary = models.CharField(max_length=150,unique=True)
 
